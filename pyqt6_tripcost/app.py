323762a37d028035f3529e6b9a67953a5fd7749b
Mey_Ethan <meye013@hsd.k12.or.us>
2024-05-09 15:28:55 -0700

    Added main layout and panels

--- pyqt6_tripcost/app.py
+++ pyqt6_tripcost/app.py
@@ -1,34 +1,45 @@
 import sys
-
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QComboBox,QVBoxLayout,QWidget,QHBoxLayout)
+from PyQt6.QtGui import QFont, QFontDatabase
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QDoubleSpinBox, QVBoxLayout ,QWidget ,QHBoxLayout, QLabel, QLayout)
 
 
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
-
-        self.setWindowTitle("My App")
         
-        layout = QVBoxLayout()
+        self.setWindowTitle("My App")
+        self.setContentsMargins(12,12,12,12)
+        self.resize(500,300)
+        
+        self.main = QWidget()
+        self.main_layout = QHBoxLayout()
+        self.main.setLayout(self.main_layout)
+        self.setCentralWidget(self.main)
+        
+        self.left_panel = QWidget()
+        self.left_panel_layout = QVBoxLayout()
+        self.left_panel.setLayout(self.left_panel_layout)
+        self.main_layout.addWidget(self.left_panel_layout)
 
-        btnlayout = QVBoxLayout()
+        self.right_panel = QWidget()
+        self.right_panel_layout = QVBoxLayout()
+        self.right_panel.setLayout(self.right_panel_layout)
+        self.main_layout.addWidget(self.right_panel_layout)
 
-        layout.addLayout(btnlayout)
+        self.title = QLabel("Trip Cost Calculator")
+        self.main_layout.addWidget(self.title)
+        font = self.title.font()
+        font.setPointSize(30)
+        self.title.setFont(font)
 
-        layout.addWidget(QPushButton("Push Me"))
 
-        Botton = QPushButton("Push Me")
-        Botton.layout = (btnlayout)
-        self.setCentralWidget(Botton)
-        Botton.setFixedSize(QSize(100,50))
-
-        self.setFixedSize(QSize(300,300))
+        
 
 
 app = QApplication(sys.argv)
-
+app.setStyle("Fusion")
 window = MainWindow()
 window.show()
 
